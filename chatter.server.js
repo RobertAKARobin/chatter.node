d0022b118d644400af1154b317c000826c47c698
RobertAKARobin <robertgfthomas@gmail.com>
2017-02-13 14:31:35 -0600

    Added Vue dependencies

--- chatter.server.js
+++ chatter.server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 
+app.use('/vendor', express.static('./node_modules'));
 app.use('/', express.static('./public'));
 
 app.listen('3000', function(){
