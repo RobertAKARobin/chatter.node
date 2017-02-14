159f7865f895351cbdd99643a8556fdf60aed9fc
RobertAKARobin <robertgfthomas@gmail.com>
2017-02-13 21:24:34 -0600

    wip

--- chatter.server.js
+++ chatter.server.js
@@ -2,9 +2,14 @@
 
 var express = require('express');
 var app = express();
+var sampleData = require('./db/sample.json');
 
 app.use('/vendor', express.static('./node_modules'));
 app.use('/', express.static('./public'));
+
+app.get('/api', function(req, res){
+	res.send(sampleData);
+});
 
 app.listen('3000', function(){
 	console.log('Running on 3000.');
